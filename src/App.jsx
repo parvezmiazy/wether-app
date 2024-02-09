ac988ef8fd9e2cacfbf593903d88443aadec242e
Parvez Ahmed <miazyparvez@gmail.com>
2024-02-10 00:32:10 +0600

    converted from html to  react

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import Header from "./components/header/Header";
+import WeatherBoard from "./components/header/weather/WeatherBoard";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="grid place-items-center h-screen">
+      <Header />
+      <main>
+        <section>
+          <WeatherBoard />
+        </section>
+      </main>
+    </div>
+  );
 }
